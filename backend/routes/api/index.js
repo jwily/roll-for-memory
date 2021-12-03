842f9b6b9e77c2ce9e7fd146ac20a5ce130c41a6
John J. Lee <johnlee1120@gmail.com>
2021-12-03 15:48:11 -0800

    Finish phase 3

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,42 @@
 const router = require('express').Router();
 
+// const asyncHandler = require('express-async-handler');
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// const { restoreUser } = require('../../utils/auth.js');
+// const { requireAuth } = require('../../utils/auth.js');
+
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body })
 })
+
+// // GET /api/set-token-cookie
+// router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'SquallLeonhart'
+//         }
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// }));
+
+// // GET /api/restore-user
+// router.get(
+//     '/restore-user',
+//     restoreUser,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
+
+// // GET /api/require-auth
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
 
 module.exports = router;
