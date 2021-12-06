e44f47192d1c86428ccd09b8cbf80ecf25dad203
John J. Lee <johnlee1120@gmail.com>
2021-12-06 13:50:07 -0800

    Added missing await in sign up store

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 
 import './SignupForm.css';
 
 function SignupFormPage() {
+    // const history = useHistory();
+
     const dispatch = useDispatch();
     const sessionUser = useSelector((state) => state.session.user);
     const [email, setEmail] = useState("");
