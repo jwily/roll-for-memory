0918361a5ea1353832a1122e3bb92fe9b583e9bf
John J. Lee <johnlee1120@gmail.com>
2021-12-06 14:18:11 -0800

    Get basic database params and associations down

--- backend/db/models/notebook.js
+++ backend/db/models/notebook.js
@@ -1,11 +1,18 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Notebook = sequelize.define('Notebook', {
-    name: DataTypes.STRING,
-    userId: DataTypes.INTEGER
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   }, {});
-  Notebook.associate = function(models) {
+  Notebook.associate = function (models) {
     // associations can be defined here
+    Notebook.belongsTo(models.User, { foreignKey: 'userId' });
   };
   return Notebook;
 };
