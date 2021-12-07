429defebda5d1a70416f4df888e8117e1eefde39
John J. Lee <johnlee1120@gmail.com>
2021-12-07 09:07:15 -0800

    Basic notes backend started and notes list display

--- frontend/src/components/NotesList/index.js
+++ frontend/src/components/NotesList/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { getNotes } from '../../store/notes';
+
 import './NotesList.css';
 
 const NotesList = () => {
+    const dispatch = useDispatch();
+
+    // const user = useSelector(state => state.session.user);
+    const notes = useSelector(state => state.notes);
+    // console.log(user);
+    console.log(notes);
+
+    useEffect(() => {
+        dispatch(getNotes())
+    }, [dispatch])
+
     return (
         <div className='notes-list'>
             <h2>Notes List</h2>
-            <p>Link 1</p>
-            <p>Link 2</p>
-            <p>Link 3</p>
+            {notes.notesOrder.map((id, idx) => {
+                return <NavLink key={idx} to={`/notes/${id}`}>{notes[id].name || `Untitled`}</NavLink>
+            })}
         </div>
     )
 }
