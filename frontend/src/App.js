89a54a0a898ea9769ae5d9a69ea3ffda1915024f
John J. Lee <johnlee1120@gmail.com>
2021-12-06 16:48:43 -0800

    First commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,14 @@ import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
+import { HomePage } from './components/Home';
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
+
+  // This checks to see if a user was checked for
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
@@ -23,6 +26,10 @@ function App() {
           </Route>
           <Route path="/signup">
             <SignupFormPage />
+          </Route>
+          {/* Below will be the main display  */}
+          <Route>
+            <HomePage />
           </Route>
         </Switch>
       )}
---
+++ frontend/src/components/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { useSelector } from 'react-redux';
+// import
+
+export const HomePage = () => {
+    return (
+        <div className='homePage'>
+            <h1>I'm Working!</h1>
+        </div>
+    )
+};
