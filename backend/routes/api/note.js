51ca971ac6db858d02dce23c1a945e8667954cea
John J. Lee <johnlee1120@gmail.com>
2021-12-06 16:47:46 -0800

    Pause on route work

--- backend/routes/api/note.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const asyncHandler = require('express-async-handler');
-
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
-
-const router = express.Router();
-
-router.get('/:id(\\d+)')
