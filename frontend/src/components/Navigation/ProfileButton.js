88c4709048338a632ba394e0785a7a3d76216129
John J. Lee <johnlee1120@gmail.com>
2021-12-08 15:55:51 -0800

    Smaller images

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
+import { useHistory } from "react-router";
 
 import './Navigation.css'
 
 function ProfileButton({ user }) {
+    const history = useHistory();
+
     const dispatch = useDispatch();
     const [showMenu, setShowMenu] = useState(false);
 
@@ -28,6 +31,7 @@ function ProfileButton({ user }) {
     const logout = (e) => {
         e.preventDefault();
         dispatch(sessionActions.logout());
+        history.push('/');
     };
 
     return (
