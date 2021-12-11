63899aa20a5799e00724aa3e0440f936edbb3d04
John J. Lee <johnlee1120@gmail.com>
2021-12-10 23:04:32 -0800

    Restore Side Link

---
+++ frontend/src/components/SideNav/SideNavLink.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const SideNavLink = ({ books, id, idx, handleDelete }) => {
+    return (
+        <div key={`book-${idx}-div`} className='book-link-div'>
+            <NavLink key={idx} to={`/notebooks/${id}`}>
+                {books[id].name}
+            </NavLink>
+            <div key={`book-${id}-btns`} className='book-link-btns'>
+                <button key={`book-${id}-edit`} type='button'>Edit</button>
+                <button key={`book-${id}-del`} type='button' onClick={(e) => handleDelete(id)}>Del</button>
+            </div>
+        </div>
+    )
+}
+
+export default SideNavLink;
