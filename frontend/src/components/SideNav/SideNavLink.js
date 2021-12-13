d84cd08bc720cf399bd3d652bca06126b8c8beb7
John J. Lee <johnlee1120@gmail.com>
2021-12-13 00:24:19 -0800

    First commit

--- frontend/src/components/SideNav/SideNavLink.js
+++ frontend/src/components/SideNav/SideNavLink.js
@@ -1,15 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { editBook } from '../../store/notebooks';
 
-const SideNavLink = ({ id, handleDelete, books }) => {
+const SideNavLink = ({ id, idx, handleDelete, books }) => {
+
+    useEffect(() => {
+        const field = document.getElementById(`${id}-edit-field`);
+        if (field) {
+            field.focus();
+        }
+    })
+
+    const dispatch = useDispatch();
+
+    const [edit, setEdit] = useState(false);
+    const [editText, setEditText] = useState(books[id].name)
+
+    // useEffect(() => {
+    //     console.log(edit);
+    // }, [edit])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (e.target.value === books[id].name) return;
+
+        const payload = {
+            bookId: id,
+            name: editText
+        }
+
+        const response = await dispatch(editBook(payload));
+
+        if ('errors' in response) {
+            console.log(response.errors);
+        } else {
+            setEdit(false);
+            return response;
+        }
+    }
 
     return (
         <div className='book-link-div'>
-            <NavLink to={`/notebooks/${id}`}>
-                {books[id].name}
-            </NavLink>
+            {!edit ?
+                <NavLink to={`/notebooks/${id}`}>
+                    {books[id].name}
+                </NavLink> :
+                <form onSubmit={handleSubmit} >
+                    <input
+                        value={editText}
+                        onChange={(e) => setEditText(e.target.value)}
+                        onBlur={(e) => {
+                            setEdit(false)
+                        }}
+                        id={`${id}-edit-field`}
+                    />
+                    <button >Submit</button>
+                </form>
+            }
+
             <div className='book-link-btns'>
-                <button type='button'>Edit</button>
+
+                <button type='button' onClick={(e) => {
+                    setEdit(!edit)
+                }}>Edit</button>
+
                 <button type='button' onClick={(e) => handleDelete(id)}>Del</button>
             </div>
         </div>
